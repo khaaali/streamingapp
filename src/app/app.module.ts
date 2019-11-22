aeab010086e5cb9b1a7241e381487af69f1c8673
rav <ram.vankamamidi@fusionsystems.de>
2019-11-22 18:32:42 +0100

    fuak all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule }    from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {StreamingService} from './streaming.service'
@@ -11,7 +10,6 @@ import { FormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule
--- src/app/streaming.service.ts
+++ src/app/streaming.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as io from 'socket.io-client';
 import { Observable } from 'rxjs';
 
