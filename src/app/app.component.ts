846dd862e4eb6c05864eda51947af25689b7101a
rav <ram.vankamamidi@fusionsystems.de>
2019-11-28 18:58:59 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,58 @@
-import { Component,OnInit, ElementRef, ViewChild } from '@angular/core';
-import {StreamingService} from './streaming.service'
-import { NgOpenCVService, OpenCVLoadResult } from 'ng-open-cv';
-import { fromEvent, Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {StreamingService} from './streaming.service';
+import {NgOpenCVService, OpenCVLoadResult} from 'ng-open-cv';
+import {fromEvent, Observable} from 'rxjs';
+import {switchMap} from 'rxjs/operators';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
 export class AppComponent {
 
-	 openCVLoadResult: Observable<OpenCVLoadResult>;
+    openCVLoadResult : Observable < OpenCVLoadResult >;
 
-  title = 'stoicksVisonApp';
-  message: string;
-    messages: string[] = [];
+    title = 'stoicksVisonApp';
+    message : string;
+    messages : string[] = [];
 
-@ViewChild('canvasOutput',{static:false})
-  canvasOutput: ElementRef;
+    @ViewChild('canvasOutput', {static: false})
+    canvasOutput : ElementRef;
 
-  constructor(
-  	private ngOpenCVService: NgOpenCVService,
-  	private streaming:StreamingService
+    constructor(private ngOpenCVService : NgOpenCVService, private streaming : StreamingService) {}
 
-  	) { }
-
-  ngOnInit() {
-    this.openCVLoadResult = this.ngOpenCVService.isReady$;
-    this.streaming
-      .getMessages()
-      .subscribe((message: string) => {
-        this.messages.push(message);
-      });
-  }
-
- loadImage(event) {
-    if (event.target.files.length) {
-      const reader = new FileReader();
-      const load$ = fromEvent(reader, 'load');
-      load$
-        .pipe(
-          switchMap(() => {
-            return this.ngOpenCVService.loadImageToHTMLCanvas("./assets/pp.jpeg", this.canvasOutput.nativeElement);
-          })
-        )
-        .subscribe(
-          () => {},
-          err => {
-            console.log('Error loading image', err);
-          }
-        );
-      reader.readAsDataURL(event.target.files[0]);
+    ngOnInit() {
+        this.openCVLoadResult = this.ngOpenCVService.isReady$;
+        this
+            .streaming
+            .getMessages()
+            .subscribe((message : string) => {
+                this
+                    .messages
+                    .push(message);
+            });
     }
-  }
- 
 
+    loadImage(event) {
+        if (event.target.files.length) {
+            const reader = new FileReader();
+            const load$ = fromEvent(reader, 'load');
+            load$.pipe(switchMap(() => {
+                return this
+                    .ngOpenCVService
+                    .loadImageToHTMLCanvas('./assets/pp.jpeg', this.canvasOutput.nativeElement);
+            })).subscribe(() => {}, err => {
+                console.log('Error loading image', err);
+            });
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    }
 
- sendMessage() {
-    this.streaming.sendMessage(this.message);
-    this.message = '';
-  }
+    sendMessage() {
+        this
+            .streaming
+            .sendMessage(this.message);
+        this.message = '';
+    }
 
- // ngOnInit() {
- //    this.streaming
- //      .getMessages()
- //      .subscribe((message: string) => {
- //        this.messages.push(message);
- //      });
- //  }
-
+    // ngOnInit() {    this.streaming      .getMessages()      .subscribe((message:
+    // string) => {        this.messages.push(message);      });  }
 
 }
--- src/app/streaming.service.ts
+++ src/app/streaming.service.ts
@@ -1,48 +1,38 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import * as io from 'socket.io-client';
-import { Observable } from 'rxjs';
-import { NgOpenCVService } from 'ng-open-cv';
+import {Observable} from 'rxjs';
+import {NgOpenCVService} from 'ng-open-cv';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 
 export class StreamingService {
 
-  	private url = 'http://localhost:3000';  // URL to web api
-  	 private socket; 
+    private url = 'http://localhost:3000'; // URL to web api
+    private socket;
 
-  constructor(private ngOpenCVService: NgOpenCVService) { 
-   this.socket = io(this.url);
-}
-
- public sendMessage(message) {
-        this.socket.emit('new-message', message);
+    constructor(private ngOpenCVService : NgOpenCVService) {
+        this.socket = io(this.url);
     }
 
-public getMessages = () => {
-        return Observable.create((observer) => {
-            this.socket.on('tcpdata', (message) => {
+    public sendMessage(message) {
+        this
+            .socket
+            .emit('new-message', message);
+    }
 
-          let len=this.readIntegerFromCurrentPos(message);
-                  console.log(len.toString());
-                  const buffer = new ArrayBuffer(len);
-                  
-                 observer.next(message);
-                 // console.log(message);
-            });
+    public getMessages = () => {
+        return Observable.create((observer) => {
+            this
+                .socket
+                .on('tcpdata', (message) => {
+
+                    let frame= new cv.Mat();
+                    console.log(frame);
+                    console.log(message);
+                    observer.next(frame);
+                    // console.log(message);
+                });
         });
     }
 
-
-  public readIntegerFromCurrentPos(buffer) {
-  let currentPos = 0;
-  var buf = Buffer.from(buffer);
-  var len = buf.readUIntLE(currentPos, currentPos + 3);
-  // currentPos+=3;
-  return len;
 }
-
-}
-
-
