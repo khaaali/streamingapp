e17dabfd31ba98c5170796623cdff8ab708236cc
khaaali <vnvnsairam@gmail.com>
2019-12-19 21:03:13 +0100

    added material icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import {StreamingService} from './streaming.service'
 import { FormsModule } from '@angular/forms';
 import { OpenCVOptions } from 'projects/ng-open-cv/src/public_api';
 import { NgOpenCVModule } from 'ng-open-cv';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
 const openCVConfig: OpenCVOptions = {
   scriptUrl: `assets/opencv/opencv.js`,
   wasmBinaryFile: 'assets/opencv/opencv_js.wasm',
@@ -23,7 +25,11 @@ const openCVConfig: OpenCVOptions = {
     BrowserModule,
      NgOpenCVModule.forRoot(openCVConfig),
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [StreamingService],
   bootstrap: [AppComponent]
